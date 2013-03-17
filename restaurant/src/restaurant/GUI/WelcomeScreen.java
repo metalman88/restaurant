369a84c029e296ebb5b8c4f87cfe8ed3db903e4f
Jesse Puente <kj369kj@gmail.com>
2013-03-17 11:17:44 -0700

    Added some methods to make the program fullscreen or back to regular size. Also added a method to switch the main content pane to a, customer table view. This way the customers are limited to the amount of functionality they can interact with.

--- restaurant/src/restaurant/GUI/WelcomeScreen.java
+++ restaurant/src/restaurant/GUI/WelcomeScreen.java
@@ -1,8 +1,11 @@
 package restaurant.GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,6 +16,8 @@ import java.awt.Color;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
 import java.awt.Font;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
@@ -35,7 +40,10 @@ import java.awt.event.KeyEvent;
 public class WelcomeScreen extends JFrame {
 
 	private JPanel contentPane;	
+	private JPanel customerContentPane;
 	private static RestaurantSystem restaurantSystem;
+	private static final int SCREEN_SIZE_X = 1024;
+	private static final int SCREEN_SIZE_Y = 720;
 	JTabbedPane tabbedPane;
 
 	/**
@@ -60,15 +68,16 @@ public class WelcomeScreen extends JFrame {
 	 */
 	public WelcomeScreen() {
 		
+		customerContentPane = new CustomerAfterLoginPanel(restaurantSystem,this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1024, 720);
+		setBounds(100, 100, SCREEN_SIZE_X, SCREEN_SIZE_Y);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 0, 1008, 682);
+		tabbedPane.setBounds(0, 0, SCREEN_SIZE_X-10, SCREEN_SIZE_Y-10);
 		contentPane.add(tabbedPane);
 		
 		
@@ -77,14 +86,63 @@ public class WelcomeScreen extends JFrame {
 		tabbedPane.addTab("Check In", null, new CheckInPanel(restaurantSystem), null);
 		tabbedPane.addTab("Server", null, new ServerPanel(restaurantSystem), null);
 		tabbedPane.addTab("Kitchen", null, new KitchenPanel(restaurantSystem), null);
+		//presentCustomerLogin();
 
 		
 	}
 	
-	public void presentCustomerLogin()
+	public void switchToFullScreen()
 	{
-		contentPane.remove(tabbedPane);
-		CustomerAfterLoginPanel CALP = new CustomerAfterLoginPanel(restaurantSystem,this);
+	    getContentPane().setPreferredSize( Toolkit.getDefaultToolkit().getScreenSize());
+	    pack();
+	    setResizable(false);
+	    show();
+
+	    SwingUtilities.invokeLater(new Runnable() {
+	      public void run()
+	      {
+	        Point p = new Point(0, 0);
+	        SwingUtilities.convertPointToScreen(p, getContentPane());
+	        Point l = getLocation();
+	        l.x -= p.x;
+	        l.y -= p.y;
+	        setLocation(l);
+	      }
+	    });
+		
+	}
+	
+	public void switchToRegularSizeScreen()
+	{
+		setResizable(true);
+		getContentPane().setPreferredSize(new Dimension(SCREEN_SIZE_X, SCREEN_SIZE_Y));
+		pack();
+		show();
+		
+	    SwingUtilities.invokeLater(new Runnable() {
+		      public void run()
+		      {
+		        Point p = new Point(0, 0);
+		        SwingUtilities.convertPointToScreen(p, getContentPane());
+		        Point l = getLocation();
+		        l.x += p.x;
+		        l.y += p.y;
+		        setLocation(l);
+		      }
+		    });
+		
+	}
+	
+	public void swapLoginCustomerPanel()
+	{
+		if(getContentPane() == contentPane)
+		{
+			setContentPane(customerContentPane);
+		}
+		else if(getContentPane() == customerContentPane)
+		{
+			setContentPane(contentPane);
+		}
 		
 	}
 }
