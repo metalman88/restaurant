a25d60bc3b4fd825708af8dd91233dfdc5e0e026
Park <Park@Park-PC>
2013-03-19 14:46:17 -0700

    Started the Kitchen Panel

--- restaurant/src/restaurant/GUI/KitchenPanel.java
+++ restaurant/src/restaurant/GUI/KitchenPanel.java
@@ -3,14 +3,23 @@ package restaurant.GUI;
 import javax.swing.JPanel;
 
 import restaurant.system.RestaurantSystem;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 public class KitchenPanel extends JPanel{
 	private RestaurantSystem restaurantSystem;
+	private JTable kitchenTable;
 	
 	public KitchenPanel(RestaurantSystem restaurantSystem)
 	{
 		this.restaurantSystem= restaurantSystem;
+		setLayout(null);
 		setUpPanel();
+		
 	}
 	
 	public void  setUpPanel()
@@ -21,6 +30,75 @@ public class KitchenPanel extends JPanel{
 		//or right click on this class and open with WindowBuilder if installed.
 		//
 		//User the restaurantSystem variable to interact with the class
+		
+		JLabel lblKitchenInterface = new JLabel("Kitchen Interface");
+		lblKitchenInterface.setBounds(169, 5, 118, 20);
+		lblKitchenInterface.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		add(lblKitchenInterface);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 63, 325, 226);
+		add(scrollPane);
+		
+		kitchenTable = new JTable();
+		kitchenTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Order ID", "Order Item", "Status"
+			}
+		));
+		kitchenTable.setToolTipText("");
+		scrollPane.setViewportView(kitchenTable);
 	}
-
 }
